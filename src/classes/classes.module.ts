545f117747a3272b025740a2de5986bdbd342cdd
Niranga <nirangak95@gmail.com>
2026-02-28 01:03:38 +0530

    add

--- src/classes/classes.module.ts
+++ src/classes/classes.module.ts
@@ -3,12 +3,12 @@ import { ClassesService } from './classes.service';
 import { ClassesController } from './classes.controller';
 import { Class } from './entities/class.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Subject } from 'rxjs';
+import { Subject } from 'src/subjects/entities/subject.entity';
 import { Grade } from 'src/grades/entities/grade.entity';
 import { Teacher } from 'src/teachers/entities/teacher.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Class, Teacher, Grade, Subject])],
+  imports: [TypeOrmModule.forFeature([Class,Subject, Teacher, Grade,])],
 
   controllers: [ClassesController],
   providers: [ClassesService],
